e7fc4f5161704026e0d361c262d0bfc1445c9919
Nick Craig-Wood <nick@craig-wood.com>
2004-09-24 17:17:04 +0000

    Unlink and bold the current page in the nav

--- top-and-tail.py
+++ top-and-tail.py
@@ -5,8 +5,8 @@ Top and tail the web page passed in
 
 """
 __author__ = "Nick Craig-Wood (nick@craig-wood.com)"
-__version__ = "$Revision: 1.4 $"
-__date__ = "$Date: 2004-09-24 17:01:32 $"
+__version__ = "$Revision: 1.5 $"
+__date__ = "$Date: 2004-09-24 17:17:04 $"
 __copyright__ = "Copyright (C) Nick Craig-Wood 2004"
 
 import sys, re, os, optparse
@@ -16,6 +16,7 @@ opt = None                              # options filled in by main()
 def menu_nav(f):
     "Given a path to an html file - reads its Nav file and returns html"
     nav = os.path.join(os.path.dirname(f), "nav")
+    basename = os.path.basename(f)
     assert os.path.exists(nav), "nav file '%s' not found" % nav
     menu = []
     for line in file(nav):
@@ -23,6 +24,8 @@ def menu_nav(f):
         path, name = line.split(None, 1)
         if name == ".":
             menu.append('<hr />')
+        elif path == basename:
+            menu.append('<b>%s</b><br />' % name)
         else:
             menu.append('<a href="%s">%s</a><br />' % (path, name))
     return "\n".join(menu)
@@ -40,7 +43,7 @@ def transform(f, top_path):
     text = re.sub(r"\s*<[Hh]1>"+title_quoted+"</[Hh]1>\s*", r"", text)
 
     # Look for CVS Id
-    # <!-- $Id: top-and-tail.py,v 1.4 2004-09-24 17:01:32 ncw Exp $ -->
+    # <!-- $Id: top-and-tail.py,v 1.5 2004-09-24 17:17:04 ncw Exp $ -->
     # NB regexp is split to stop CVS substituting it!
     match = re.search(r"\$" + r"Id: (.*?),v ([0-9.]+) (\d\d\d\d)/(\d\d)/(\d\d) \d\d:\d\d:\d\d (\S+).*?\$", text)
     assert match, "Couldn't find CVS date in %s" % f
