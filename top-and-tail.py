2959df2c6c3409bd0e8ace220c879249667ef514
Nick Craig-Wood <nick@craig-wood.com>
2006-05-03 10:46:29 +0000

    Adapt to new CVS date format

--- top-and-tail.py
+++ top-and-tail.py
@@ -5,8 +5,8 @@ Top and tail the web page passed in
 
 """
 __author__ = "Nick Craig-Wood (nick@craig-wood.com)"
-__version__ = "$Revision: 1.6 $"
-__date__ = "$Date: 2004-10-07 09:38:25 $"
+__version__ = "$Revision: 1.7 $"
+__date__ = "$Date: 2006-05-03 10:46:29 $"
 __copyright__ = "Copyright (C) Nick Craig-Wood 2004"
 
 import sys, re, os, optparse
@@ -43,9 +43,9 @@ def transform(f, top_path):
     text = re.sub(r"\s*<[Hh]1>"+title_quoted+"</[Hh]1>\s*", r"", text)
 
     # Look for CVS Id
-    # <!-- $Id: top-and-tail.py,v 1.6 2004-10-07 09:38:25 ncw Exp $ -->
+    # <!-- $Id: top-and-tail.py,v 1.7 2006-05-03 10:46:29 ncw Exp $ -->
     # NB regexp is split to stop CVS substituting it!
-    match = re.search(r"\$" + r"Id: (.*?),v ([0-9.]+) (\d\d\d\d)/(\d\d)/(\d\d) \d\d:\d\d:\d\d (\S+).*?\$", text)
+    match = re.search(r"\$" + r"Id: (.*?),v ([0-9.]+) (\d\d\d\d)[-/](\d\d)[-/](\d\d) \d\d:\d\d:\d\d (\S+).*?\$", text)
     assert match, "Couldn't find CVS date in %s" % f
     cvs_file, cvs_rev, cvs_year, cvs_month, cvs_day, cvs_who = match.groups()
 
