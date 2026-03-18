0cce2e1a7d85524751660532d8957d6fc563733c
Nick Craig-Wood <nick@craig-wood.com>
2026-03-18 15:28:50 +0000

    Remove Bootstrap, jQuery, and Popper dependencies
    Replace ~325KB of JS/CSS libraries with ~150 lines of custom CSS
    and ~30 lines of vanilla JS. The navbar, grid, cards, dropdowns,
    and utilities are now implemented directly in custom.css/custom.js.

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,21 +1,56 @@
 // Site JS
 
-$(function() {
+document.addEventListener("DOMContentLoaded", function() {
   // Add hover links on headings
-  $("h2, h3, h4, h5, h6").each(function(i, el) {
-    var $el, icon, id;
-    $el = $(el);
-    id = $el.attr('id');
-    icon = '<i class="fa fa-link"></i>';
+  document.querySelectorAll("h2, h3, h4, h5, h6").forEach(function(el) {
+    var id = el.getAttribute("id");
     if (id) {
-      return $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
+      var a = document.createElement("a");
+      a.className = "header-link";
+      a.href = "#" + id;
+      a.innerHTML = '<i class="fa fa-link"></i>';
+      el.prepend(a);
     }
   });
+
   // Wire up copy to clipboard buttons
-  $(".copy-to-clipboard").click(function() {
-    var copyText = $(this).prev();
-    copyText.select();
-    document.execCommand("copy");
-    $(this).attr("title", "Copied!");
+  document.querySelectorAll(".copy-to-clipboard").forEach(function(btn) {
+    btn.addEventListener("click", function() {
+      var copyText = btn.previousElementSibling;
+      copyText.select();
+      document.execCommand("copy");
+      btn.title = "Copied!";
+    });
+  });
+
+  // Navbar toggle
+  var toggler = document.querySelector(".navbar-toggler");
+  var collapse = document.getElementById("navbarSupportedContent");
+  if (toggler && collapse) {
+    toggler.addEventListener("click", function() {
+      collapse.classList.toggle("show");
+    });
+  }
+
+  // Dropdown toggle (mobile)
+  document.querySelectorAll(".dropdown-toggle").forEach(function(toggle) {
+    toggle.addEventListener("click", function(e) {
+      if (window.innerWidth < 768) {
+        e.preventDefault();
+        var menu = toggle.nextElementSibling;
+        if (menu && menu.classList.contains("dropdown-menu")) {
+          menu.classList.toggle("show");
+        }
+      }
+    });
+  });
+
+  // Close dropdowns when clicking outside
+  document.addEventListener("click", function(e) {
+    if (!e.target.closest(".dropdown")) {
+      document.querySelectorAll(".dropdown-menu.show").forEach(function(menu) {
+        menu.classList.remove("show");
+      });
+    }
   });
 });
